419d05b10ac5b5455ff3c61f54d2f8781307bca5
steflamb <71817448+steflamb@users.noreply.github.com>
2021-12-07 13:23:03 +0100

    Added fid every tick

--- src/metrics/metric_defaults.py
+++ src/metrics/metric_defaults.py
@@ -12,6 +12,7 @@ from dnnlib import EasyDict
 
 metric_defaults = EasyDict([(args.name, args) for args in [
     EasyDict(name='fid50k',    func_name='metrics.frechet_inception_distance.FID', num_images=50000, minibatch_per_gpu=8),
+    EasyDict(name='fid10k',    func_name='metrics.frechet_inception_distance.FID', num_images=10000, minibatch_per_gpu=8),
     EasyDict(name='is50k',     func_name='metrics.inception_score.IS',             num_images=50000, num_splits=10, minibatch_per_gpu=8),
     EasyDict(name='ppl_zfull', func_name='metrics.perceptual_path_length.PPL',     num_samples=50000, epsilon=1e-4, space='z', sampling='full', crop=True, minibatch_per_gpu=4, Gs_overrides=dict(dtype='float32', mapping_dtype='float32')),
     EasyDict(name='ppl_wfull', func_name='metrics.perceptual_path_length.PPL',     num_samples=50000, epsilon=1e-4, space='w', sampling='full', crop=True, minibatch_per_gpu=4, Gs_overrides=dict(dtype='float32', mapping_dtype='float32')),
