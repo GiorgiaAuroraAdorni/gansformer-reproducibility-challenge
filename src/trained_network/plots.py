bc360eed3b5b9a71f0fda26bda5ece1bae5fe0cd
GiorgiaAuroraAdorni <adornigiorgia@outlook.it>
2021-12-10 10:38:27 +0100

    Added FID score for GANformer with Simplex attention and discriminator attention

--- src/trained_network/plots.py
+++ src/trained_network/plots.py
@@ -36,8 +36,9 @@ def plot_learning_curve(data, labels, title):
     plt.xlabel("Step")
     plt.ylabel("FID score")
 
-    plt.ylim(bottom=-20, top=450)
+    # plt.semilogy()
     plt.xlim(left=-20, right=320)
+    plt.ylim(bottom=-20, top=450)
     plt.axis('tight')
 
     plt.grid()
@@ -52,14 +53,18 @@ def plot_learning_curve(data, labels, title):
 
 # opening the CSV file
 data_style = pd.read_csv('scores/FIDScore_Stylegan2_300kimg.csv', delimiter=" ")
-data_ganfsimpnat = pd.read_csv('scores/FIDScore_GanformerSimplexNoAtt_300kimg.csv', delimiter=" ")
+data_ganfsimpnatt = pd.read_csv('scores/FIDScore_GanformerSimplexNoAtt_300kimg.csv', delimiter=" ")
+data_ganfsimpatt = pd.read_csv('scores/FIDScore_GanformerSimplexAtt_300kimg.csv', delimiter=" ")
 
-labels = ["StyleGAN2", "GANformer with Simplex attention \n(no discriminator attention)"]
+labels = ["StyleGAN2",
+          "GANformer with Simplex attention \n(no discriminator attention)",
+          "GANformer with Simplex attention \n(and discriminator attention)"]
+
 title = "Learning Performance"
 # Cross validation with 100 iterations to get smoother mean test and train
 # score curves, each time with 20% data randomly selected as a validation set.
 
-plot_learning_curve([data_style, data_ganfsimpnat], labels, title)
+plot_learning_curve([data_style, data_ganfsimpnatt, data_ganfsimpatt], labels, title)
 
 plt.savefig('out_imgs/FIDscore.pdf', dpi=600)
 plt.close()
